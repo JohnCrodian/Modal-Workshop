2eb5e6c5fdd1a58b677b0709130f8272d2a9017c
John Crodian <johncrodian24@gmail.com>
2016-11-16 16:01:41 -0500

    First commit

---
+++ modal.js
@@ -0,0 +1,28 @@
+//Get the modal
+var modal = document.getElementById("myModal");
+
+//Get the button that opens the modal
+// var btn = document.getElementById("myBtn")
+
+//Get the <span> element that closes the modal
+var close = document.getElementsByClassName("close")[0];
+
+//When the user clicks the button, open the modal
+// btn.onclick = function() {
+// 	modal.style.display = "block";
+// }
+
+//When the user clicks on <span> (x), close the modal
+
+close.onclick = function () {
+	modal.style.display = "none";
+}
+
+//Timed popup function
+
+function popUp () {
+var modal = document.getElementById("myModal");
+	modal.style.display = "block"
+}
+
+window.setTimeout(popUp, 3000);
